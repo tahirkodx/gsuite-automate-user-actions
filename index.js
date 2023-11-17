e500e2ad20e1289c349f3bbe88ed9f52a61c17fd
adilKodx <adil@Kodxsystem.com>
2023-11-17 19:43:30 +0500

    ca

--- index.js
+++ index.js
@@ -105,6 +105,6 @@ app.post("/", async (req, res) => {
 });
 
 
-app.listen(80, () => {
-  console.log("Server is running on http://localhost:80");
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
 });
